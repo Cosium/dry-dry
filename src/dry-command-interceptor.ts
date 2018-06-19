fd82830d1f74e95886143038c45034fddd0914e5
ng16fe1 <pascal.treilhes@soprasteria.com>
2018-06-19 02:12:29 +0200

    Another batch of lint fixes

--- src/dry-command-interceptor.ts
+++ src/dry-command-interceptor.ts
@@ -15,7 +15,7 @@ export class DryCommandInterceptor {
      * @param {Cli} cli The CLI to use
      * @param {NodeJS.Process} process The main process
      */
-    constructor(private readonly cli: Cli, process: Process) {
+    constructor(cli: Cli, process: Process) {
         this.rawArgs = process.argv.slice(2);
         this.npmCommandProxy = new NpmCommandProxy(cli, this.rawArgs);
     }
@@ -28,12 +28,12 @@ export class DryCommandInterceptor {
 
         const intercept: boolean = this.rawArgs.length > 0 && this.drySpecificArgs.indexOf(this.rawArgs[0]) > -1;
 
-        if (intercept){
+        if (intercept) {
             const arg1: string = this.rawArgs[0];
             const arg2: string = this.rawArgs.length > 1 ? this.rawArgs[1] : undefined;
 
-            switch( arg1 ) {
-                case this.drySpecificArgs[0]:{// saveTo
+            switch (arg1) {
+                case this.drySpecificArgs[0]: {// saveTo
                     return this.saveTo(arg2);
                 }
             }
@@ -43,8 +43,8 @@ export class DryCommandInterceptor {
         }
     }
 
-    private saveTo(target:string):Promise<void> {
-        const packageJson: Object = JSON.parse(fs.readFileSync(path.resolve('./package.json'), 'utf8'));
+    private saveTo(target: string): Promise<void> {
+        const packageJson: object = JSON.parse(fs.readFileSync(path.resolve('./package.json'), 'utf8'));
         fs.writeFileSync(path.resolve(target), JSON.stringify(packageJson, null, 2) + '\n');
         return Promise.resolve();
     }
